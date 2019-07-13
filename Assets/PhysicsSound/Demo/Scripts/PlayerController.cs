2d46daaf721127421783335c05978eaef82b0f09
Daniel Williams <danielw093@gmail.com>
2019-07-13 16:15:55 +0100

    Did some further setup

---
+++ Assets/PhysicsSound/Demo/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace PhysicsSound.Demo.Scripts
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerController : MonoBehaviour
+    {
+        private Rigidbody _rigidbody;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+    }
+}
